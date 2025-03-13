9ad116e716594708bebd2b5b0c412a3b556557ee
MicheleLafronza <michele.lafronza91@gmail.com>
2025-03-13 19:11:40 +0100

    feat: dashboard name change to bacheca, bacheca style start to transform

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,8 @@
-<x-layouts.app :title="__('Dashboard')">
+{{-- <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                {{-- <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" /> --}}
-                Giochi?
+                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
@@ -14,6 +13,23 @@
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+        </div>
+    </div>
+</x-layouts.app> --}}
+
+
+
+{{-- TEST OUT  --}}
+
+<x-layouts.app :title="__('Bacheca')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="flex flex-row gap-4 justify-between">
+            <div class="min-h-[200px] min-w-[200px] rounded-xl overflow-hidden border border-neutral-200 dark:border-neutral-700 p-4">
+                <flux:heading size="lg">{{ __('Giochi a cui stai giocando') }}</flux:heading>
+                <flux:subheading>{{ __('Attualmente non stai giocando a nessun gioco') }}</flux:subheading>
+            </div>
+            <div class="min-h-[200px] min-w-[200px] bg-sky-700 rounded-xl"></div>
+            <div class="min-h-[200px] min-w-[200px] bg-sky-700 rounded-xl"></div>
         </div>
     </div>
 </x-layouts.app>
